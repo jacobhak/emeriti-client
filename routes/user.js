77d2ac8f266593754115cc0b8c8db4badbdc0880
Simon Strom <simon.strom@gmail.com>
2013-02-24 01:57:59 +0100

    list users

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,30 @@
 
 exports.list = function(req, res){
-  res.render ('user_index.jade',
-		{ title: "userlist"});
+  var sys = require('util'),
+    rest = require('restler');
+
+  var url = "http://rocky-mountain-1049.herokuapp.com/users";
+  rest.get(url).on('complete', function(data) {
+    res.render ('user_index.jade',
+      { title: "userlist", users: data});
+  });
 };
 
 exports.newUser = function(req, res) {
-	res.render('new_user.jade', {title: "ny user"});
+  res.render('new_user.jade', {title: "ny user"});
 }
 
 exports.create = function(req, res) {
-	var sys = require('util'),
+  var sys = require('util'),
     rest = require('restler');
 
-    
-	var jsonData = req.body;
-	var url = "http://rocky-mountain-1049.herokuapp.com/users";
-	console.log(jsonData);
-	rest.postJson(url, jsonData).on('complete', function(data, response) {
-		console.log ('user created');
-	});
-	res.send("post user");
+  var jsonData = req.body;
+  var url = "http://rocky-mountain-1049.herokuapp.com/users";
+  console.log(jsonData);
+  
+  rest.postJson(url, jsonData).on('complete', function(data, response) {
+    console.log ('user created');
+  });
+  res.send("post user");
 }
 
