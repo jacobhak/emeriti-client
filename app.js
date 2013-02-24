e8874c4eee162586712254714eeea323cd8eb671
Simon Strom <simon.strom@gmail.com>
2013-02-24 03:54:35 +0100

    add oauth, flash and fixed redirect

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , flash = require('connect-flash');
 
 var app = express();
 
 app.configure(function(){
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({ cookie: { maxAge: 60000 }}));
+  app.use(flash());
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
